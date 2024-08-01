7e006c7ec764f0097fdd87be0f564029b5b22830
zitiso <zitiso@gmail.com>
2024-08-01 12:13:01 -0700

    Add Graphics App

---
+++ MyGraphicsApp/app/src/main/java/com/example/mygraphicsapp/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.mygraphicsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mygraphicsapp.ui.theme.MyGraphicsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyGraphicsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Draw()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Draw() {
+    Column {
+        Text("Hello, Graphics!",
+            modifier = Modifier
+//                .fillMaxSize()
+                .drawBehind {
+                    drawRect(color = Color.Green, size = Size(50f, 300f))
+                }
+        )
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(width = 10.dp, color = Color.Red)
+        )
+        {
+            drawRect(
+                color = Color.LightGray,
+                size = Size(50f, 150f)
+            )
+//            drawLine(
+//                Color.Red,
+//                Offset(50f, 50f),
+//                Offset(400f, 600f),
+//                strokeWidth = 25f
+//            )
+//            drawLine(
+//                Brush.horizontalGradient(
+//                    listOf(Color.Red, Color.Blue)
+//                ),
+//                Offset(500f, 300f),
+//                Offset(1000f, 300f),
+//                strokeWidth = 100f,
+//                cap = StrokeCap.Round,
+//            )
+//            drawCircle(Color.Red)
+//
+//            drawCircle(
+//                Color.Blue,
+//                radius = size.minDimension / 3.3f,
+//                alpha = 0.5f // blend with red circle and produce purple
+//            )
+//
+//            drawCircle(
+//                Color.Blue,
+//                radius = size.minDimension / 8.0f,
+//                alpha = 1f // overwrite red and blue circles
+//            )
+//            drawPoints(
+//                listOf(
+//                    Offset(50f, 350f),
+//                    Offset(100f, 345f),
+//                    Offset(150f, 310f),
+//                    Offset(200f, 200f),
+//                    Offset(250f, 280f),
+//                    Offset(300f, 140f),
+//                    Offset(350f, 100f),
+//                    Offset(400f, 180f),
+//                    Offset(450f, 220f),
+//                    Offset(500f, 330f)
+//                ),
+//                PointMode.Points,
+//                Color.Red,
+//                strokeWidth = 30.0f,
+//                cap = StrokeCap.Round
+//            )
+        }
+
+    }
+
+}
